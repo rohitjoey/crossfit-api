b61d391b997d0580160e0bc6a15e0459fa91167f
rohitjoey <roht.praz@gmail.com>
2022-05-16 12:40:57 +0545

    Add home page
    For "/" send home

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -10,9 +10,9 @@ app.use(express.json());
 const v1WorkoutRouter = require("./v1/routes/workout.routes");
 const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
 
-// app.get("/", (req, res) => {
-//   res.send("<h2>Hello<h2>");
-// });
+app.get("/", (req, res) => {
+  res.send("<h2>Hello<h2>");
+});
 
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
